ef440cac60534e679d42b90973acb75097952513
Marcell <35337794+marcellvries@users.noreply.github.com>
2023-04-01 10:38:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function calculateRisk() {
   const stoplossPercent = (1- (stoplossPrice / entryPrice));
   
   // Calculate order quantity
-  const orderQuantity = ((portfolioBalance * (maxLossPerTrade / 100)) / (stoplossPercent * 100) );
+  const orderQuantity = ((portfolioBalance * (maxLossPerTrade / 100)) / stoplossPercent );
   
   // Calculate margin
   const margin = entryPrice * orderQuantity / leverage;
