6c1ccaf006c1d20479ff178b1efecf65bd016914
Marcell <35337794+marcellvries@users.noreply.github.com>
2023-04-01 20:38:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function calculateRisk() {
       ]
     },
     options: {
-      responsive: true,
+      responsive: false,
       maintainAspectRatio: true,
       spanGaps: true,
       scales: {
